4d97b0347650423444c31813da2fec38f0f11981
Shpetim Selaci <shselaci3@gmail.com>
2022-03-23 14:13:21 +0100

    improve game layout, add game states

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,24 +1,67 @@
 import React, { useRef, useEffect } from 'react'
 
 import useGameState from './hooks/useGameState'
-import { Ball, GameState, Player } from './types'
+import { Ball, GamePlayState, GameState, Player } from './types'
 
 let MAX_FPS = 60
-
+let PADDING = 20
 const drawPlayerPaddle = (ctx: CanvasRenderingContext2D, paddle: Player) => {
-  ctx.fillRect(paddle.X, paddle.Y, paddle.Width, paddle.Height)
+  ctx.fillRect(
+    paddle.X + PADDING / 2,
+    paddle.Y - paddle.Height / 2 + PADDING / 2,
+    paddle.Width,
+    paddle.Height
+  )
   ctx.fillStyle = '#fff'
-  ctx.closePath()
 }
 
 const drawBall = (ctx: CanvasRenderingContext2D, ball: Ball) => {
   ctx.beginPath()
 
-  ctx.arc(ball.X, ball.Y, ball.Radius, 0, Math.PI * 2)
+  ctx.arc(ball.X + PADDING, ball.Y + PADDING / 2, ball.Radius, 0, Math.PI * 2)
   ctx.fillStyle = '#fff'
 
   ctx.fill()
   ctx.closePath()
+}
+const drawBorders = (ctx: CanvasRenderingContext2D) => {
+  ctx.strokeRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  ctx.strokeStyle = 'black'
+  ctx.lineWidth = 20
+}
+
+const GamePlayStateTexts: Partial<Record<GamePlayState, string>> = {
+  [GamePlayState.StartState]: 'START GAME / PRESS SPACE',
+  [GamePlayState.GameOverState]: 'GAME OVER / PRESS SPACE to restart',
+}
+
+const drawGameState = (ctx: CanvasRenderingContext2D, gameState: GameState) => {
+  const textToRender = GamePlayStateTexts[gameState.State as GamePlayState]
+  if (textToRender) {
+    ctx.font = '20px Arial'
+
+    ctx.fillStyle = 'whitesmoke'
+    ctx.fillText(
+      textToRender,
+      ctx.canvas.width / 2 - textToRender.length * 5,
+      200,
+      400
+    )
+  }
+}
+
+const drawStats = (
+  ctx: CanvasRenderingContext2D,
+  gameState: GameState,
+  fps: number
+) => {
+  ctx.font = '16px Arial'
+  ctx.fillText(`Fps: ${fps}`, 50, 50, 400)
+  ctx.fillText(`Level: ${gameState.Level}`, 110, 50, 400)
+  ctx.fillText(`Rally: ${gameState.Rally}`, 175, 50, 400)
+  ctx.fillText(`MaxScore: ${gameState.MaxScore}`, 240, 50, 400)
+  ctx.fillText(`Player 1: ${gameState.Player1.Score}`, 350, 50, 400)
+  ctx.fillText(`Player 2: ${gameState.Player2.Score}`, 430, 50, 400)
 }
 
 const Canvas: React.FC<
@@ -41,10 +84,14 @@ const Canvas: React.FC<
       const currentFps = Math.round(1000 / (now - beRef.current))
       beRef.current = now - (diff % interval)
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-      ctx.fillText(String(currentFps), 100, 100, 200)
+
+      drawBorders(ctx)
       drawPlayerPaddle(ctx, gameStateRef.current.Player1)
       drawPlayerPaddle(ctx, gameStateRef.current.Player2)
       drawBall(ctx, gameStateRef.current.Ball)
+      drawStats(ctx, gameStateRef.current, currentFps)
+
+      drawGameState(ctx, gameStateRef.current)
     }
   }
 
@@ -68,7 +115,14 @@ const Canvas: React.FC<
     }
   }, [])
 
-  return <canvas ref={canvasRef} {...props} width={800} height={600} />
+  return (
+    <canvas
+      ref={canvasRef}
+      {...props}
+      width={800 + PADDING}
+      height={600 + PADDING}
+    />
+  )
 }
 
 export default Canvas
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,9 @@ export type GameState = typeof stateMock
 export type Player = GameState['Player1']
 
 export type Ball = GameState['Ball']
+
+export enum GamePlayState {
+  StartState = 0,
+  PlayState = 1,
+  GameOverState = 2,
+}
